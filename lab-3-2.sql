aded3472b737137f7f06027bc6a30d08694b4656
Sneha Chelimilla <sneha.chelimilla@kellogg.northwestern.edu>
2024-04-05 19:54:03 +0000

    Completed lab-3-2

--- lab-3-2.sql
+++ lab-3-2.sql
@@ -39,4 +39,4 @@
 -- | Washington Nationals          | 78.0             |
 -- +-------------------------------+------------------+
 
-
+select name, year, avg(wins) from teams where year>2000  group by name;
